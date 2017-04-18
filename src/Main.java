e2087300221ab72084b17b10bc2d5d266b5e7947
douglas-cp <douglas-cp@douglas_pge>
2017-04-18 07:46:20 -0300

    - Correção na indexação do arquivo utilizando os metadados default do solr. - Melhora na remoção de index.

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,47 @@
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
 
+import org.apache.pdfbox.pdfparser.PDFParser;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.util.PDFTextStripper;
 import org.apache.solr.client.solrj.SolrClient;
 import org.apache.solr.client.solrj.impl.HttpSolrClient;
+import org.apache.solr.client.solrj.request.ContentStreamUpdateRequest;
 import org.apache.solr.common.SolrInputDocument;
+import org.apache.solr.common.util.NamedList;
 
-import com.lowagie.text.pdf.PdfReader;
+import com.itextpdf.text.pdf.PdfReader;
+
+import gov.goias.pge.resources.PDFLayoutTextStripper;
 
 public class Main {
-	@SuppressWarnings("unchecked")
 	public static void main(String[] args) throws Exception{
+		/*
+		SolrClient client = new HttpSolrClient.Builder("http://10.6.56.150:8983/solr/PGE_CEJUR").build();
+		ContentStreamUpdateRequest req = new ContentStreamUpdateRequest("/update/extract");
+		File file = null;
+		file = new File("C:\\Users\\douglas-cp\\Documents\\Javadesenv\\intellij\\configs_sicop\\solr\\Sem.pdf");
+		req.addFile(file, "");
+		req.setParam("extractOnly", "false");
+		NamedList<Object> result = client.request(req);
+		client.commit();
+		client.close();
+		*/
+		deleteIndex("", "/mnt/disco02/CEJUR/inicial - acao civil publica cc improbidade administrativa - pro saude - lrs - assinado.pdf");
+	}
+	
+	public static void indexFile() throws Exception{
 		try{
-			String urlString = "http://10.6.56.150:8983/solr/PGE_CEJUR/";
-			String nomeArquivo = "acordo_ortografico.pdf";
-			
+			String core = "PGE_CEJUR";
+			String nomeArquivo = "arquivo upload.pdf";
+			String urlString = "http://10.6.56.150:8983/solr/"+core+"/";
+			nomeArquivo = nomeArquivo.replace("+", " ");
 			File file = null;
 			file = new File("C:\\Users\\douglas-cp\\Documents\\Javadesenv\\intellij\\configs_sicop\\solr\\" + nomeArquivo);
 			if(!file.exists()){
@@ -22,21 +50,51 @@ public class Main {
 			
 			PdfReader reader = new PdfReader(file.getAbsolutePath());
 			HashMap<String, String> info = reader.getInfo();
-
+	
 			SolrClient solr = new HttpSolrClient.Builder(urlString).build();
 			SolrInputDocument document = new SolrInputDocument();
-			document.addField("id", "/mnt/disco02/CEJUR/"+nomeArquivo);
+			document.addField("id", "C:\\Users\\douglas-cp\\Documents\\Javadesenv\\intellij\\configs_sicop\\solr\\"+nomeArquivo);
 			document.addField("name", nomeArquivo);
-			document.addField("titulo",  info.get("Title"));
-			document.addField("assunto",  info.get("Subject"));
-			document.addField("autor",  info.get("Author"));
-			document.addField("palavrachave",  info.get("Keywords"));
+			document.addField("title",  info.get("Title"));
+			document.addField("subject",  info.get("Subject"));
+			document.addField("author",  info.get("Author"));
+			document.addField("keywords",  info.get("Keywords"));
 			document.addField("uploader",  info.get("Uploader"));
+			document.addField("created",  info.get("Created"));
+			document.addField("stream_size",  info.get("stream_size"));
+			document.addField("fullText", "");
+			
+	        try {
+	        	StringBuilder string = new StringBuilder();
+	            PDFParser pdfParser = new PDFParser(new FileInputStream("C:\\Users\\douglas-cp\\Documents\\Javadesenv\\intellij\\configs_sicop\\solr\\arquivo upload.pdf"));
+	            pdfParser.parse();
+	            PDDocument pdDocument = new PDDocument(pdfParser.getDocument());
+	            PDFTextStripper pdfTextStripper = new PDFLayoutTextStripper();
+	            string.append(pdfTextStripper.getText(pdDocument));
+	            document.addField("content", string);
+	            document.addField("stored", true);
+	        } catch (FileNotFoundException e) {
+	            e.printStackTrace();
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
 			solr.add(document);
 			solr.commit();
 			solr.close();
 		}catch(Exception e ){
 			throw new Exception("Erro ao index arquivo -> "+e.getMessage());
+		} 
+	}
+	
+	public static void deleteIndex(String core, String id) throws Exception{
+		try{
+			String urlString = "http://10.6.56.150:8983/solr/PGE_CEJUR/";
+			SolrClient solr = new HttpSolrClient.Builder(urlString).build();
+			solr.deleteById(id);
+			solr.commit();
+			solr.close();
+		}catch(Exception e ){
+			throw new Exception("Erro ao deletar index -> "+e.getMessage());
 		}
 	}
 }
--- src/gov/goias/pge/resources/SolarService.java
+++ src/gov/goias/pge/resources/SolarService.java
@@ -1,7 +1,6 @@
 package gov.goias.pge.resources;
 
 import java.io.File;
-import java.util.HashMap;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -10,9 +9,8 @@ import javax.ws.rs.Produces;
 
 import org.apache.solr.client.solrj.SolrClient;
 import org.apache.solr.client.solrj.impl.HttpSolrClient;
-import org.apache.solr.common.SolrInputDocument;
-
-import com.lowagie.text.pdf.PdfReader;
+import org.apache.solr.client.solrj.request.ContentStreamUpdateRequest;
+import org.apache.solr.common.util.NamedList;
 
 @Path("/solarservice")
 public class SolarService {
@@ -24,6 +22,7 @@ public class SolarService {
 		try{
 			String urlString = "http://10.6.56.150:8983/solr/PGE_CEJUR/";
 			SolrClient solr = new HttpSolrClient.Builder(urlString).build();
+			id = id.replaceAll("_", " ");
 			solr.deleteById("/mnt/disco02/CEJUR/"+id);
 			solr.commit();
 			solr.close();
@@ -39,29 +38,21 @@ public class SolarService {
 	public void indexFile(@PathParam("core") String core, @PathParam("nomeArquivo") String nomeArquivo) throws Exception{
 		try{
 			String urlString = "http://10.6.56.150:8983/solr/"+core+"/";
-
+			nomeArquivo = nomeArquivo.replace("+", " ");
 			File file = null;
-			file = new File("/mnt/disco02/CEJUR/" + nomeArquivo);
-			System.out.println("/mnt/disco02/CEJUR/" + nomeArquivo);
+			//file = new File("/mnt/disco02/CEJUR/" + nomeArquivo);
+			file = new File("C:\\Users\\douglas-cp\\Documents\\Javadesenv\\intellij\\configs_sicop\\solr\\" + nomeArquivo);
 			if(!file.exists()){
 				throw new Exception("Arquivo não encontrado.");
 			}
 			
-			PdfReader reader = new PdfReader(file.getAbsolutePath());
-			HashMap<String, String> info = reader.getInfo();
-	
-			SolrClient solr = new HttpSolrClient.Builder(urlString).build();
-			SolrInputDocument document = new SolrInputDocument();
-			document.addField("id", "/mnt/disco02/CEJUR/"+nomeArquivo);
-			document.addField("name", nomeArquivo);
-			document.addField("titulo",  info.get("Title"));
-			document.addField("assunto",  info.get("Subject"));
-			document.addField("autor",  info.get("Author"));
-			document.addField("palavrachave",  info.get("Keywords"));
-			document.addField("uploader",  info.get("Uploader"));
-			solr.add(document);
-			solr.commit();
-			solr.close();
+			SolrClient client = new HttpSolrClient.Builder(urlString).build();
+			ContentStreamUpdateRequest req = new ContentStreamUpdateRequest("/update/extract");
+			req.addFile(file, "");
+			req.setParam("extractOnly", "false");
+			NamedList<Object> result = client.request(req);
+			client.commit();
+			client.close();
 		}catch(Exception e ){
 			throw new Exception("Erro ao index arquivo -> "+e.getMessage());
 		}
