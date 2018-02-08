5d257f74331b96fd434f6b568171c806f3006980
douglazcp <douglazcp@gmail.com>
2018-02-08 16:59:03 -0200

    - Melhora no retorno de cada método com descrição de sucesso ou nao e uma descrição de erro no formato JSON.

--- src/gov/goias/pge/resources/SolarService.java
+++ src/gov/goias/pge/resources/SolarService.java
@@ -1,53 +1,59 @@
 package gov.goias.pge.resources;
 
-import java.io.File;
-
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-
 import org.apache.solr.client.solrj.SolrClient;
 import org.apache.solr.client.solrj.impl.HttpSolrClient;
 import org.apache.solr.client.solrj.request.ContentStreamUpdateRequest;
 import org.apache.solr.common.util.NamedList;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import java.io.File;
+
 @Path("/solarservice")
 public class SolarService {
+
+	public enum Codigo {
+		SUCESSO, ERRO
+	}
+
+	@Path("/")
+	@GET
+	@Produces("application/json")
+	public String index(){
+		return montaRespostaJson(Codigo.SUCESSO.toString(), "index","");
+	}
 
 	@Path("/delete/{core}/{id}")
 	@GET
 	@Produces("application/json")
-	public void deleteIndex(@PathParam("core") String core, @PathParam("id") String id) throws Exception {
+	public String deleteIndex(@PathParam("core") String core, @PathParam("id") String id){
 		try {
-			String urlString = "http://10.6.56.150:8983/solr/PGE_CEJUR/";
+			String urlString = "http://10.6.56.150:8983/solr/"+core+"/";
 			SolrClient solr = new HttpSolrClient.Builder(urlString).build();
 			id = id.replaceAll("_", " ");
-			solr.deleteById("/mnt/disco02/CEJUR/" + id);
+			solr.deleteById(id);
 			solr.commit();
 			solr.close();
+			return montaRespostaJson(Codigo.SUCESSO.toString(),"deleteIndex("+core+","+id+")","");
 		} catch (Exception e) {
-			throw new Exception("Erro ao deletar index -> " + e.getMessage());
+			return montaRespostaJson(Codigo.ERRO.toString(),"deleteIndex("+core+","+id+")",e.getMessage());
 		}
 	}
 
 	@SuppressWarnings("unchecked")
-	@Path("/index/{core}/{nomeArquivo}")
+	@Path("/indexFile/{core}/{nomeArquivo}")
 	@GET
 	@Produces("application/json")
-	public void indexFile(@PathParam("core") String core, @PathParam("nomeArquivo") String nomeArquivo) throws Exception {
+	public String indexFile(@PathParam("core") String core, @PathParam("nomeArquivo") String nomeArquivo){
 		try {
-			String urlString = "http://10.6.56.150:8983/solr/" + core + "/";
+			String urlString = "http://10.6.56.150:8983/solr/"+core+"/";
 			nomeArquivo = nomeArquivo.replace("+", " ");
-			File file = null;
-			file = new File("/mnt/disco02/CEJUR/" + nomeArquivo);
-			// file = new
-			// File("C:\\Users\\douglas-cp\\Documents\\Javadesenv\\intellij\\configs_sicop\\solr\\"
-			// + nomeArquivo);
+			File file = new File(nomeArquivo);
 			if (!file.exists()) {
-				throw new Exception("Arquivo não encontrado.");
+				throw new Exception("Arquivo "+nomeArquivo+" não encontrado");
 			}
-
 			SolrClient client = new HttpSolrClient.Builder(urlString).build();
 			ContentStreamUpdateRequest req = new ContentStreamUpdateRequest("/update/extract");
 			req.addFile(file, "");
@@ -55,8 +61,19 @@ public class SolarService {
 			NamedList<Object> result = client.request(req);
 			client.commit();
 			client.close();
+			return montaRespostaJson(Codigo.SUCESSO.toString(),"indexFile("+core+","+nomeArquivo+")","");
 		} catch (Exception e) {
-			throw new Exception("Erro ao index arquivo -> " + e.getMessage());
+			return montaRespostaJson(Codigo.ERRO.toString(),"indexFile("+core+","+nomeArquivo+")",e.getMessage());
 		}
+	}
+
+	private String montaRespostaJson(String codigo, String metodo, String descricao){
+		String json = "";
+		json+= "{ ";
+		json+= "	\"resultado\":\""+codigo+"\", ";
+		json+= "	\"metodo\": \""+metodo+"\", ";
+		json+= "	\"descricao\":\""+descricao+"\" ";
+		json+= "} ";
+		return json;
 	}
 }
