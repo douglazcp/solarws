f4818dab80ca41ad306e83d6d6af4c4a0a34636d
douglazcp@gmail.com <git#1234>
2018-02-26 11:17:31 -0300

    - Melhora no layout do json. - Explicação de chamada dos métodos.

--- src/gov/goias/pge/resources/SolarService.java
+++ src/gov/goias/pge/resources/SolarService.java
@@ -2,8 +2,11 @@ package gov.goias.pge.resources;
 
 import Util.DataUtil;
 import org.apache.solr.client.solrj.SolrClient;
+import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.client.solrj.impl.HttpSolrClient;
 import org.apache.solr.client.solrj.request.ContentStreamUpdateRequest;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.common.SolrDocumentList;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -28,14 +31,15 @@ public class SolarService {
 	@Produces("application/json")
 	public String index(){
 		String json = "";
-
         json += " {";
         json += "     \"metodos\": {";
         json += "         \"deleteIndex\": {";
-        json += "             \"parametros\":\"core,id\"";
+        json += "             \"parametros\":\"core/id\",";
+        json += "			  \"url\":\"/solarws/solarservice/deleteIndex/core/id\"";
         json += "         },";
         json += "         \"indexFile\": {";
-        json += "             \"parametros\":\"core,nomeArquivo\"";
+        json += "             \"parametros\":\"core/nomeArquivo\",";
+		json += "			  \"url\":\"/solarws/solarservice/indexFile/core/id\"";
         json += "         }";
         json += "     },";
         json += "     \"retorno\":{";
@@ -43,7 +47,7 @@ public class SolarService {
         json += "             \"metodo\":\"METODO INVOCADO\",";
         json += "             \"descricao\": \"ERRO\"";
         json += "     },";
-        json += " \"versao\":\"1.0\" ";
+        json += " \"versao\":\"1.2\" ";
         json += " }";
 
 		return json;
@@ -55,13 +59,24 @@ public class SolarService {
 	@Produces("application/json")
 	public String indexFile(@PathParam("core") String core, @PathParam("nomeArquivo") String nomeArquivo){
 		try {
-			String urlString = "http://10.6.56.150:8983/solr/"+core+"/";
 			nomeArquivo = nomeArquivo.replace("+", " ");
 			//file = new File("/mnt/disco02/CEJUR/" + nomeArquivo);
-			File file = new File(montaCaminhoFisicoArquivo(nomeArquivo, core));
+			//File file = new File(montaCaminhoFisicoArquivo(nomeArquivo, core));
+			File file = new File("/home/douglas-cp/Documents/pge/workspace/envriroment/configs_sicop/solr/2018/02/"+nomeArquivo);
 			if (!file.exists()) {
 				throw new Exception("Arquivo "+nomeArquivo+" não encontrado");
 			}
+
+			String urlString = "http://10.6.56.150:8983/solr/"+core+"/";
+			HttpSolrClient solr = new HttpSolrClient.Builder(urlString).build();
+			ContentStreamUpdateRequest req = new ContentStreamUpdateRequest("/update/extract");
+			req.addFile(file, "");
+			req.setParam("extractOnly", "false");
+			req.setParam("literal.id", nomeArquivo);
+			solr.request(req);
+			solr.commit();
+			solr.close();
+			/*
 			SolrClient client = new HttpSolrClient.Builder(urlString).build();
 			ContentStreamUpdateRequest req = new ContentStreamUpdateRequest("/update/extract");
 			req.addFile(file, "");
@@ -69,24 +84,37 @@ public class SolarService {
 			client.request(req);
 			client.commit();
 			client.close();
+			*/
 			return montaRespostaJson(Codigo.SUCESSO.toString(),"indexFile("+core+","+nomeArquivo+")","");
 		} catch (Exception e) {
 			return montaRespostaJson(Codigo.ERRO.toString(),"indexFile("+core+","+nomeArquivo+")",e.getMessage());
 		}
 	}
 
-	@Path("/delete/{core}/{id}")
+	@Path("/deleteIndex/{core}/{id}")
 	@GET
 	@Produces("application/json")
 	public String deleteIndex(@PathParam("core") String core, @PathParam("id") String id){
 		try {
 			String urlString = "http://10.6.56.150:8983/solr/"+core+"/";
 			SolrClient solr = new HttpSolrClient.Builder(urlString).build();
+			/*
 			id = montaCaminhoFisicoArquivo(id.replaceAll("_", " "), core);
+
+
+			*/
 			solr.deleteById(id);
 			solr.commit();
+			SolrQuery query = new SolrQuery();
+			query.set("q", "id:"+id);
+			QueryResponse response = solr.query(query);
 			solr.close();
-			return montaRespostaJson(Codigo.SUCESSO.toString(),"deleteIndex("+core+","+id+")","");
+			SolrDocumentList docList = response.getResults();
+			if(String.valueOf(docList.getNumFound()).equals("0")) {
+				return montaRespostaJson(Codigo.SUCESSO.toString(), "deleteIndex(" + core + "," + id + ")", "");
+			}else{
+				throw new Exception("Index não removido.");
+			}
 		} catch (Exception e) {
 			return montaRespostaJson(Codigo.ERRO.toString(),"deleteIndex("+core+","+id+")",e.getMessage());
 		}
